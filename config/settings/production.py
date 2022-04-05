9e7975d1b151bf9ede2c7649df3dbcfe4d8539b9
Andy Woods <andytwoods@users.noreply.github.com>
2022-04-05 17:54:34 +0100

    whitenoise issue

--- config/settings/production.py
+++ config/settings/production.py
@@ -36,7 +36,7 @@ SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
 
 # STATIC
 # ------------------------
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "config.storage.CompressedManifestStaticFilesStorage"
 # MEDIA
 # ------------------------------------------------------------------------------
 
